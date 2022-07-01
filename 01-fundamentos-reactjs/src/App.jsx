d81e42e3890caa4805c689a4801b0b09bf9845f3
Giossaurus <gihhskt@gmail.com>
2022-06-30 22:24:46 -0300

    header and css modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,13 @@
 // JSX = Javascript + XML 
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Giovanni Della Dea" 
         content = "Lorem ipsum dolor sit amet consectetur" 
