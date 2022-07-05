2c2a601d931cf6c167003d65b1cfad700974e9ba
Giossaurus <gihhskt@gmail.com>
2022-07-04 23:40:20 -0300

    stylings other comments

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -36,8 +37,14 @@ export function Post() {
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
-                
             </form>
+
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </article>
     )
 }
