09ad2f0bb5c414f8f1fdd1585787bd0c0ff2e171
Giossaurus <gihhskt@gmail.com>
2022-07-04 23:37:34 -0300

    post comment section buttons

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/giossaurus.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Giovanni Della Dea</strong>
+                            <time tittle="11 de Maio às 08:13h "dateTime="2022=05-11 08:13:30"> Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Lorem ipsum, dolor sit amet.</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Apladuir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
