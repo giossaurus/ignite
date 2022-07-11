1a29d7164fe1cfa3a64418174756816341a2e6b7
Giossaurus <gihhskt@gmail.com>
2022-07-11 17:29:22 -0300

    Update App.jsx

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -7,6 +7,35 @@ import styles from './App.module.css'
 
 import './global.css';
 
+const posts = [
+  { 
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋', },
+      { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content:'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2022-07-04 20:00:00'),
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋', },
+      { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content:'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2022-07-10 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
