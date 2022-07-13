fa079157eedac7ab5fc3abff7278b881bba0a726
Giossaurus <gihhskt@gmail.com>
2022-07-12 22:42:18 -0300

    Update App.jsx

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -28,7 +28,7 @@ const posts = [
       role: 'Educator @ Rocketseat',
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋', },
+      { type: 'paragraph', content: '👋', },
       { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
       { type: 'link', content:'jane.design/doctorcare', },
     ],
