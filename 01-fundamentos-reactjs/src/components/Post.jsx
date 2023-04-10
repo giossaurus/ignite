cee54cc6e139db09a824653c028d5b629a139b1e
Giossaurus <gihhskt@gmail.com>
2023-04-09 22:35:21 -0300

    Update Post.jsx

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -2,18 +2,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(props) {
-
-    console.log(props);
-
+export function Post({ author }) {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/giossaurus.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Giovanni Della Dea</strong>
-                        <span>Front End Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
